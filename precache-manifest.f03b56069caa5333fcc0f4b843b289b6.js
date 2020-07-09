998e1c70e51ec706dfdb2063e2edf833805593c9
aryagowri <arya.gowri@gmail.com>
2020-07-09 19:07:08 -0400

    Updates

---
+++ precache-manifest.f03b56069caa5333fcc0f4b843b289b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3201a61a5b9aff906f8ccf6d668ce792",
+    "url": "/aryagowri/sample-react-app-repo/index.html"
+  },
+  {
+    "revision": "26da902ff8e883847706",
+    "url": "/aryagowri/sample-react-app-repo/static/css/main.d901578a.chunk.css"
+  },
+  {
+    "revision": "56604769e86cadfbd450",
+    "url": "/aryagowri/sample-react-app-repo/static/js/2.b61f442e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/aryagowri/sample-react-app-repo/static/js/2.b61f442e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26da902ff8e883847706",
+    "url": "/aryagowri/sample-react-app-repo/static/js/main.75f112af.chunk.js"
+  },
+  {
+    "revision": "a1db4b4b5ec082a05630",
+    "url": "/aryagowri/sample-react-app-repo/static/js/runtime-main.c585f2d1.js"
+  },
+  {
+    "revision": "3bf052473e49b8a7ebb39d73fafe59d2",
+    "url": "/aryagowri/sample-react-app-repo/static/media/carousel-img-1.3bf05247.jpg"
+  },
+  {
+    "revision": "02a224378814aad98889874cbc946da3",
+    "url": "/aryagowri/sample-react-app-repo/static/media/carousel-img-2.02a22437.jpg"
+  },
+  {
+    "revision": "f08f80d0d85069192ec8d45c61d6bfd0",
+    "url": "/aryagowri/sample-react-app-repo/static/media/carousel-img-3.f08f80d0.jpg"
+  }
+]);
